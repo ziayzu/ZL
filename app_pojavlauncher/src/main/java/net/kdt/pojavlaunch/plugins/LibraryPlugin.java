29be6d69a2711a1896cad277fd7c7c4051c8d5ac
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-09-05 22:23:31 +0400

    feat[plugin]: implement common library provider plugin
    AnglePlugin and FFmpeg plugins are very similar

---
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/plugins/LibraryPlugin.java
@@ -0,0 +1,79 @@
+package net.kdt.pojavlaunch.plugins;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LibraryPlugin {
+    private static final String TAG = "LibraryPlugin";
+
+    private static Map<String, LibraryPlugin> loadedPlugins = new HashMap<>();
+    private String appId;
+    private String libraryPath;
+    private boolean pluginAvailable = false;
+    private LibraryPlugin(Context ctx, String app){
+        this.appId = app;
+        Log.i(TAG, "Discovering plugin " + appId);
+        try {
+            PackageInfo pluginPackage = ctx.getPackageManager().getPackageInfo(appId, PackageManager.GET_SHARED_LIBRARY_FILES);
+            this.libraryPath = pluginPackage.applicationInfo.nativeLibraryDir;
+            pluginAvailable = true;
+
+        } catch (Exception e){
+            Log.e(TAG, "Plugin discover failed: " + e.getMessage());
+        }
+    }
+
+    public static LibraryPlugin discoverPlugin(Context ctx, String appId){
+        // Do not recreate plugin instance if it was discovered before
+       if(loadedPlugins.containsKey(appId))
+           return loadedPlugins.get(appId);
+       LibraryPlugin plugin = new LibraryPlugin(ctx, appId);
+       loadedPlugins.put(appId, plugin);
+       return plugin;
+    }
+    public static LibraryPlugin getPlugin(String appId){
+        return loadedPlugins.get(appId);
+    }
+    public static boolean isAvailable(String appId){
+        return loadedPlugins.get(appId) != null && loadedPlugins.get(appId).isPluginAvailable();
+    }
+
+    public boolean isPluginAvailable() {
+        return pluginAvailable;
+    }
+
+    public String getId(){
+        return appId;
+    }
+
+    public String getLibraryPath(){
+        return libraryPath;
+    }
+    public String resolveAbsolutePath(String library) {
+        return new File(libraryPath, library).getAbsolutePath();
+    }
+
+    public boolean checkLibraries(String... libs){
+        for(String lib : libs){
+            if(!(new File(libraryPath, lib).exists())) return false;
+        }
+        return true;
+    }
+
+    public enum KnownPlugins {
+        ANGLE_PLUGIN("git.mojo.angle"),
+        FFMPEG_PLUGIN("net.kdt.pojavlaunch.ffmpeg")
+        ;
+
+        public final String id;
+        KnownPlugins(String id){
+            this.id = id;
+        }
+    }
+}
